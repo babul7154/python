eb942d4176ca0cf476807e7b0652abb6f360403c
babul7154 <babul7154it@gmail.com>
2019-07-21 00:02:50 +0530

    all fine no bug

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey('auth.User',on_delete=models.CASCADE,)
-    body = models.TextField()
-    def __str__(self):
-        return self.title
+class BookInfo(models.Model):
+    book_name=models.CharField(max_length=100)
+    author_name=models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    published_yr=models.DateField()
+    rating=models.IntegerField()
+class uread(models.Model):
+    uread_rating=models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+    publisher=models.CharField(max_length=200)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import BookInfo
+class BlogListView(ListView):
+    model = BookInfo
+    template_name = "home.html"
