5714cc156a3f159d812e6986702042a93dc3f2b4
babul7154 <babul7154it@gmail.com>
2019-07-20 10:37:16 +0530

    ok

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE,)
     body = models.TextField()
     def __str__(self):
