ddebd4f8c73b42cb05ab1998fb5a5d219e0317b7
babul7154 <babul7154it@gmail.com>
2019-08-02 21:37:57 +0530

    fine

---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from  django.urls import path
+from .views import  home,about
+urlpatterns = [
+    path("",home.as_view(),name='home'),
+    path('about/',about.as_view(),name='about'),
+]
---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic  import  TemplateView
+
+class  home(TemplateView):
+    template_name = "home.html"
+
+class about(TemplateView):
+    template_name = "about.html"
+
