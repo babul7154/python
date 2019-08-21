c35a0083173b66c5ba9635be6d4b316d658e99c2
babul7154 <babul7154it@gmail.com>
2019-08-21 08:56:07 +0530

    final

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,13 @@
 from  django.shortcuts  import   render
 
 # Create your views here.from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import CreateView
+from django import forms
+from django.views.generic import CreateView
 from django.contrib.auth.forms import  UserCreationForm
 from django.urls import reverse_lazy
-from django.views import generic
-
 
 class SignUpView(CreateView):
-    form_class=UserCreationForm  #/  it must be in  subclass
+    form_class=UserCreationForm  #/  UsercreationForm must be in  subclass
     template_name='templates/signup.html'
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),
     path('accounts/', include('accounts.urls'))
-    #!  note --if  I use  path('accounts/', include('accounts.urls'))  all acoounts url  will start  from http://127.0.0.1:8000/accounts/xyz/
-    #!  if  I use   path('accounts/', include('accounts.urls'))  all acoounts url  will start  from http://127.0.0.1:8000/xyz/  
-    #? big difference na
+    #!  note --if  I use  path('accounts/', include('accounts.urls'))  all accounts url  will start  from http://127.0.0.1:8000/accounts/xyz/
+    #!  if  I use   path('accounts/', include('accounts.urls'))  all accounts url  will start  from http://127.0.0.1:8000/xyz/  
+    #? big difference nah
 ]
