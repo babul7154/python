e2a2bd3e118b66d3f93beaff9b9e381105e29de7
babul7154 <babul7154it@gmail.com>
2019-09-14 20:31:50 +0530

    fine

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    NOT_RATED=0
+    RATED_G=1
+    RATED_PG=2
+    RATED_ADULT=3
+    RATINGS=(
+        (NOT_RATED,'NR-Not Rated'),
+        (RATED_G,'G-General Audience'),
+        (RATED_PG,'PG-Parental Guidance'),
+        (RATED_ADULT,'ADULT-Adult Audience'),
+    )
+    title=models.CharField(max_length=45)
+    rating=models.IntegerField(choices=RATINGS,default=NOT_RATED)
+    plot=models.TextField()
+    year=models.DateField()
+    runtime=models.PositiveIntegerField()
+    website=models.URLField(blank=True)
+    def __str__(self):
+        return self.title
--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Movie
+from django.views.generic import ListView,DetailView
+
+
+class MovieListView(ListView):
+    model = Movie
+    template_name = "home.html"
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+    template_name = "detail_view.html"
+
